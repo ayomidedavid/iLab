5759a553d49ab820831dee8c8774244b7526f7c0
ayomidedavid <ayomidedavid72@gmail.com>
2025-09-04 15:23:42 +0100

    Update: user approval workflow, dashboard templates, .gitignore, and admin creation instructions

--- lab_inventory_backend/app/__init__.py
+++ lab_inventory_backend/app/__init__.py
@@ -1,15 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
-from config import Config
+import config
 
-# Initialize extensions
 app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+app.secret_key = config.SECRET_KEY
 
-from app import models, routes
+from app import routes
--- lab_inventory_backend/config.py
+++ lab_inventory_backend/config.py
@@ -1,6 +1,10 @@
 import os
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'mysql://user:password@localhost/lab_inventory')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+# Flask secret key
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your_secret_key')
+
+# PyMySQL connection settings
+MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
+MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+MYSQL_DB = os.environ.get('MYSQL_DB', 'lab_inventory_system')
